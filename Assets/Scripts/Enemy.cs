440fb8db38fb67e9b443fb631bae3607d68338c2
Arthaqz <arthaqs@protonmail.com>
2020-03-17 00:17:33 +0100

    update3

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     // test xxxzzzzxz
+    // saa
     [SerializeField] private string name;
     [SerializeField] private float maxHP;
     
