bc845b7f8ce8d91770e5b452ac452868bdb6d7d3
Arthaqz <arthaqs@protonmail.com>
2020-03-21 18:56:10 +0100

    update

---
+++ Assets/Scripts/UI/LevelCompleted.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace UI
+{
+    public class LevelCompleted : MonoBehaviour
+    {
+        public static LevelCompleted Instance;
+        
+        [SerializeField] private TextMeshProUGUI levelCompletedText;
+
+        private void Awake()
+        {
+            Instance = this;
+        }
+
+        public void ShowText(string levelName)
+        {
+            levelCompletedText.gameObject.SetActive(true);
+            levelCompletedText.text = "LEVEL " + levelName + " COMPLETED";
+        }
+    }
+}
---
+++ Assets/Scripts/World/Heart.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+namespace World
+{
+    public class Heart : MonoBehaviour
+    {
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                GameSettings.LevelCompleted = true;
+                GameSettings.PlayerInvincible = true;
+                GameSettings.PlayerControlsEnabled = false;
+                GameSettings.CameraControlEnabled = false;
+            }
+        }
+    }
+}
