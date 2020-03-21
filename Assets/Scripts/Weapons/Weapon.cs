fc10abd4095e4e31fd2795bdec0791f03739e5b3
Arthaqz <arthaqs@protonmail.com>
2020-03-21 18:09:05 +0100

    update

---
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+    public class Weapon : MonoBehaviour
+    {
+        public float offset;
+
+        public GameObject projectile;
+
+        public Transform shotPoint;
+        
+        public float timeBtwShots;
+        public float startTimeBtwShots;
+    }
