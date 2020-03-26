e0297e0002e865c5b2ee3676261774cf95a6cbd1
Arthaqz <arthaqs@protonmail.com>
2020-03-26 18:23:38 +0100

    update

---
+++ Assets/Scripts/Characters/Character.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+
+public class Character : MonoBehaviour
+{
+    public GameObject playerDiedEffect;
+
+    protected void DeadEffect()
+    {
+        Instantiate(playerDiedEffect, transform.position, Quaternion.identity, TempParent.Instance.transform);
+    }
+}
